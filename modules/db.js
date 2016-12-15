d1fd3346a7510ce6ec6471ad5242e2f6398cad2c
Dmitriy Gajewski <dmit8815@gmail.com>
2016-12-15 04:27:07 +0300

    Backend - getting filelist from firebase - calculating number table and pushing result to firebase - saving files to uploads - server responses with calculated sum in format { sum: n }

--- modules/db.js
+++ modules/db.js
@@ -8,19 +8,38 @@ const config = {
 };
 
 firebase.initializeApp(config);
+firebase.database.enableLogging(true);
+let ref = firebase.database().ref("documents");
 
 module.exports = {
-    getSnapshot: function () {
+    getDocuments: function () {
         return new Promise(function (resolve, reject) {
-            let ref = firebase.database().ref("documents");
             ref.once("value")
                 .then(function(snapshot) {
-                    console.log(snapshot.val());
-                    resolve(snapshot.val())
+                    let val = snapshot.val();
+                    resolve(Object.keys(val).map(function(elem){
+                        let doc = val[elem];
+                        doc.id = elem;
+                        return doc
+                    }))
                 })
                 .catch(function (err) {
                     reject(err);
                 });
         });
+    },
+    addDocument: function (filename, path, sum) {
+        return new Promise(function (resolve, reject) {
+            ref.push({
+                name: /(.*).(xlsx|xls)$/g.exec(filename)[1] || "unnamed",
+                file: filename,
+                path: path,
+                sum: sum
+            }).then(function () {
+                resolve(sum);
+            }).catch(function (err) {
+                reject(err);
+            });
+        })
     }
 };
--- routes/docs.js
+++ routes/docs.js
@@ -1,15 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../modules/db");
+const xlsx2json = require("xlsx2json");
+const formidable = require("formidable");
 
-router.get("/", function(req, res, next) {
-    db.getSnapshot()
+router.get("/", function (req, res, next) {
+    db.getDocuments()
         .then(function (data) {
-          res.json(data);
+            res.json(data);
         })
         .catch(function (err) {
-          res.json(err);
+            res.json(err);
         })
 });
 
+router.post("/", function (req, res, next) {
+    let form = new formidable.IncomingForm();
+    form.uploadDir = "./uploads";
+    form.keepExtensions = true;
+    console.log("post file");
+
+    form.on("error", function (err) {
+        res.status(500).json(err);
+    });
+
+    form.on("file", function (name, file) {
+        xlsx2json("./" + file.path, {
+            dataStartingRow: 2,
+            mapping: {
+                "col_1": "A"
+            }
+        })
+            .then(function (jsonArray) {
+                   return db.addDocument(file.name, file.path, jsonArray[0].reduce(function (prev, cur) {
+                            return Number(prev.col_1 || prev) + Number(cur.col_1 || cur);
+                        })
+                    )
+                }
+            )
+            .then(function (sum) {
+                res.json({
+                    sum: sum
+                });
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    form.parse(req);
+});
 module.exports = router;
