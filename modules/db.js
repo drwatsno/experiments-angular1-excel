9e401a07891ad98b07a897c2b73379acd04c7259
Dmitriy Gajewski <dmit8815@gmail.com>
2016-12-18 18:53:43 +0300

    storing options (including firebase config) in separate module

--- modules/db.js
+++ modules/db.js
@@ -1,13 +1,6 @@
 const firebase = require("firebase");
-const config = {
-    apiKey: "AIzaSyB4pVApRXYJEWGkCayn0uAndq8-fQbJqSQ",
-    authDomain: "angular1-excel.firebaseapp.com",
-    databaseURL: "https://angular1-excel.firebaseio.com",
-    storageBucket: "angular1-excel.appspot.com",
-    messagingSenderId: "1087272487347"
-};
 
-firebase.initializeApp(config);
+firebase.initializeApp(require("../options").firebase);
 firebase.database.enableLogging(true);
 let ref = firebase.database().ref("documents");
 
